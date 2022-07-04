97fab81d2fe7bcb559464f4cdc8770b54737422e
Pathik Ghugare <pathik.g@somaiya.edu>
2022-07-04 21:54:58 +0530

    temporarly removed tasks

--- week-03-orchestration/prefect_flow.py
+++ week-03-orchestration/prefect_flow.py
@@ -15,7 +15,9 @@ import mlflow
 from prefect import flow, task
 from prefect.task_runners import SequentialTaskRunner
 
-@task
+# @task
+
+
 def read_dataframe(filename):
     df = pd.read_parquet(filename)
 
@@ -29,10 +31,12 @@ def read_dataframe(filename):
 
     categorical = ['PULocationID', 'DOLocationID']
     df[categorical] = df[categorical].astype(str)
-    
+
     return df
 
-@task
+# @task
+
+
 def add_features(df_train, df_val):
     # df_train = read_dataframe(train_path)
     # df_val = read_dataframe(val_path)
@@ -40,10 +44,11 @@ def add_features(df_train, df_val):
     print(len(df_train))
     print(len(df_val))
 
-    df_train['PU_DO'] = df_train['PULocationID'] + '_' + df_train['DOLocationID']
+    df_train['PU_DO'] = df_train['PULocationID'] + \
+        '_' + df_train['DOLocationID']
     df_val['PU_DO'] = df_val['PULocationID'] + '_' + df_val['DOLocationID']
 
-    categorical = ['PU_DO'] #'PULocationID', 'DOLocationID']
+    categorical = ['PU_DO']  # 'PULocationID', 'DOLocationID']
     numerical = ['trip_distance']
 
     dv = DictVectorizer()
@@ -60,7 +65,9 @@ def add_features(df_train, df_val):
 
     return X_train, X_val, y_train, y_val, dv
 
-@task
+# @task
+
+
 def train_model_search(train, valid, y_val):
     def objective(params):
         with mlflow.start_run():
@@ -98,7 +105,9 @@ def train_model_search(train, valid, y_val):
     )
     return
 
-@task
+# @task
+
+
 def train_best_model(train, valid, y_val, dv):
     with mlflow.start_run():
 
@@ -117,7 +126,7 @@ def train_best_model(train, valid, y_val, dv):
         booster = xgb.train(
             params=best_params,
             dtrain=train,
-            num_boost_round=100,
+            num_boost_round=10,
             evals=[(valid, 'validation')],
             early_stopping_rounds=50
         )
@@ -128,19 +137,26 @@ def train_best_model(train, valid, y_val, dv):
 
         with open("models/preprocessor.b", "wb") as f_out:
             pickle.dump(dv, f_out)
-        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+        mlflow.log_artifact("models/preprocessor.b",
+                            artifact_path="preprocessor")
 
         mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
 
-@flow(task_runner=SequentialTaskRunner())
-def main(train_path: str="./data/green_tripdata_2021-01.parquet",
-        val_path: str="./data/green_tripdata_2021-02.parquet"):
+# @flow(task_runner=SequentialTaskRunner())
+
+
+@flow
+def main(train_path: str = "./data/green_tripdata_2021-01.parquet",
+         val_path: str = "./data/green_tripdata_2021-02.parquet"):
     mlflow.set_tracking_uri("sqlite:///mlflow.db")
     mlflow.set_experiment("nyc-taxi-experiment")
     X_train = read_dataframe(train_path)
     X_val = read_dataframe(val_path)
-    X_train, X_val, y_train, y_val, dv = add_features(X_train, X_val).result()
+    X_train, X_val, y_train, y_val, dv = add_features(X_train, X_val)
     train = xgb.DMatrix(X_train, label=y_train)
     valid = xgb.DMatrix(X_val, label=y_val)
-    train_model_search(train, valid, y_val)
+    # train_model_search(train, valid, y_val)
     train_best_model(train, valid, y_val, dv)
+
+
+main()
